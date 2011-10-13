bfa3aea43d493d54c2fddfcd508ea5b658179108
bellbind <bellbind@gmail.com>
2011-10-13 19:45:23 +0900

    [impl] receive SEND Intent then send VIEW Intent

--- src/net/bellbind/alcsearch/AlcSearch.java
+++ src/net/bellbind/alcsearch/AlcSearch.java
@@ -1,6 +1,8 @@
 package net.bellbind.alcsearch;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class AlcSearch extends Activity
@@ -10,6 +12,18 @@ public class AlcSearch extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        //setContentView(R.layout.main);
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        if (action.equals(Intent.ACTION_SEND)) {
+            Bundle extras = intent.getExtras();
+            CharSequence data = extras.getCharSequence(Intent.EXTRA_TEXT);
+            //String url = "https://encrypted.google.com/#q=" + data;
+            String url = "http://eow.alc.co.jp/" + data + "/UTF-8/?ref=sa";
+            Uri uri = Uri.parse(url);
+            Intent sender = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(sender);
+        }
+        finish();
     }
 }
