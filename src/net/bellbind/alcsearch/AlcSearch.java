a62a13442af0e6719ab4d6aec4c041e1245b6a7f
bellbind <bellbind@gmail.com>
2011-10-13 19:41:10 +0900

    [init]

---
+++ src/net/bellbind/alcsearch/AlcSearch.java
@@ -0,0 +1,15 @@
+package net.bellbind.alcsearch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlcSearch extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
