726e3ce9a701f38f534c8c1b36dbfba37c44ea20
bellbind <bellbind@gmail.com>
2011-10-18 20:30:58 +0900

    [add] respond to WEB_SEARCH intent

--- src/net/bellbind/alcsearch/AlcSearch.java
+++ src/net/bellbind/alcsearch/AlcSearch.java
@@ -1,6 +1,7 @@
 package net.bellbind.alcsearch;
 
 import android.app.Activity;
+import android.app.SearchManager;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,10 +21,18 @@ public class AlcSearch extends Activity
             CharSequence data = extras.getCharSequence(Intent.EXTRA_TEXT);
             //String url = "https://encrypted.google.com/#q=" + data;
             String url = "http://eow.alc.co.jp/" + data + "/UTF-8/?ref=sa";
-            Uri uri = Uri.parse(url);
-            Intent sender = new Intent(Intent.ACTION_VIEW, uri);
-            startActivity(sender);
+            openUri(url);
+        } else if (action.equals(Intent.ACTION_WEB_SEARCH)) {
+            String data = intent.getStringExtra(SearchManager.QUERY);
+            String url = "http://eow.alc.co.jp/" + data + "/UTF-8/?ref=sa";
+            openUri(url);
         }
         finish();
+    }
+    
+    private void openUri(String url) {
+        Uri uri = Uri.parse(url);
+        Intent sender = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(sender);
     }
 }
